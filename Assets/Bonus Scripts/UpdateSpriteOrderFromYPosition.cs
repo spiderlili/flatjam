4e7bbee04339f643dcda4e713135a24215d6f9fb
Jing Chun Tan <jtan1993@gmail.com>
2018-01-25 21:52:44 +0000

    first

---
+++ Assets/Bonus Scripts/UpdateSpriteOrderFromYPosition.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class UpdateSpriteOrderFromYPosition : MonoBehaviour {
+
+    SpriteRenderer sprite;
+    public float orderMultiplication = -10;
+    public Transform thisObjectsGroundPosition;
+
+	void Awake()
+    {
+        sprite = GetComponent<SpriteRenderer>();
+        if (thisObjectsGroundPosition == null)
+            thisObjectsGroundPosition = transform;
+    }
+	
+	
+	void Update ()
+    {
+        if (thisObjectsGroundPosition == null)
+            Awake();
+
+
+        sprite.sortingOrder = Mathf.RoundToInt(thisObjectsGroundPosition.position.y * orderMultiplication);
+    }
+}
